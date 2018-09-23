a9d63bc5482ba805d44ada7b0e28825b3e474c91
kumataahh <153236205@qq.com>
2018-09-23 10:38:28 +0800

    update Array

--- 1 Array_List 数组列表/list_and_tdArray_read.py
+++ 1 Array_List 数组列表/list_and_tdArray_read.py
@@ -1,16 +1,16 @@
-#列表和二维数组读取一行或一列的方法
+# 列表和二维数组读取一行或一列的方法
 
 sample_list = [[67, 22,  3, 81, 25],
                [13, 78, 94, 70, 68],
                [98, 27,  3, 69, 37],
                [50, 69, 40, 86, 33]]
 
-#获取3、4行
+# 获取3、4行
 hang_3 = sample_list[2]
 hang_4 = sample_list[3]
 
-#获取3、4列
-#我们需要用列表解析的方法读取列：
+# 获取3、4列
+# 我们需要用列表解析的方法读取列：
 lie_3 = [x[2] for x in sample_list]
 lie_4 = [x[3] for x in sample_list]
 
@@ -20,7 +20,7 @@ print(hang_4)
 print(lie_3)
 print(lie_4)
 
-#而对于数组，可以直接读取：
+# 而对于数组，可以直接读取：
 import numpy as np
 lie_3 = np.array(sample_list)
 print(lie_3[:,2])
--- 1 Array_List 数组列表/python_Built-in_methods.py
+++ 1 Array_List 数组列表/python_Built-in_methods.py
@@ -1,12 +1,13 @@
-#函数上会标明该方法的时间复杂度
-#动态数组的类
+# 函数上会标明该方法的时间复杂度
+# 动态数组的类
+import ctypes
 
 class DynamicArray:
     
     def __init__ (self):
-        'Create an empty array.'
-        self._n = 0 #size
-        self._capacity = 10    #先给个10
+        # Create an empty array
+        self._n = 0   # size
+        self._capacity = 10    # 先给个10
         self._A = self._make_array(self._capacity)
         
     def __len__ (self):
@@ -23,7 +24,7 @@ class DynamicArray:
        
     # O(1) 
     def append(self, obj):
-        if self._n == self._capacity:    #首先要判断该容器是否放得下
+        if self._n == self._capacity:    # 首先要判断该容器是否放得下
             self._resize(2 * self._capacity)
         self._A[self._n] = obj    
         self._n += 1
@@ -42,7 +43,7 @@ class DynamicArray:
     def insert(self, k, value):
         if self._n == self._capacity:
             self._resize(2 * self._capacity)
-        for j in range(self._n, k, -1):    #从后往前一个一个往后移
+        for j in range(self._n, k, -1):    # 从后往前一个一个往后移
             self._A[j] = self._A[j-1]
         self._A[k] = value
         self._n += 1
@@ -50,9 +51,9 @@ class DynamicArray:
     # O(n)    
     def remove(self, value):
         for k in range(self._n):
-            if self._A[k] == value:     #一个个查value
+            if self._A[k] == value:     # 一个个查value
                 for j in range(k, self._n - 1):
-                    self._A[j] = self._A[j+1]   ##再一个个移上来
+                    self._A[j] = self._A[j+1]   # 再一个个移上来
                 self._A[self._n - 1] = None
                 self._n -= 1
                 return
@@ -63,16 +64,17 @@ class DynamicArray:
             print(self._A[i], end = ' ')
         print()
 
-
-mylist = DynamicArray()
-print ('size was: ', str(len(mylist)))
-mylist.append(10)
-mylist.append(20)
-mylist.append(30)
-mylist.insert(0, 0)
-mylist.insert(1, 5)
-mylist.insert(3, 15)
-mylist._print()
-mylist.remove(20)
-mylist._print()
-print ('size is: ', str(len(mylist)))
+# 测试
+if __name__ == '__main__':
+    mylist = DynamicArray()
+    print ('size was: ', str(len(mylist)))
+    mylist.append(10)
+    mylist.append(20)
+    mylist.append(30)
+    mylist.insert(0, 0)
+    mylist.insert(1, 5)
+    mylist.insert(3, 15)
+    mylist._print()
+    mylist.remove(20)
+    mylist._print()
+    print ('size is: ', str(len(mylist)))
