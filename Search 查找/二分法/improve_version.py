0a3d12d6476dd47b5921401ecde9b461dfcc7c56
kumataahh <153236205@qq.com>
2018-10-08 10:01:40 +0800

    update binarysearch

--- Search 查找/二分法/improve_version.py
+++ Search 查找/二分法/improve_version.py
@@ -5,16 +5,16 @@
 
 改进版本经典方法，这种方法更加好写
 把常规方法的right = mid -1 和left = mid +1修改
-找不到返回-1
+找不到返回None
 '''
 
 def bi_search2(lis, num):
    
-    #这个程序中这一条判断边界非常重要
+    # 这个程序中这一条判断边界非常重要
     if len(lis) == 0:
         return -1
     
-    #判断二分条件
+    # 判断二分条件
     left, right = 0, len(lis) - 1
     while left + 1 < right:
         mid = left + (right - left) // 2
@@ -25,15 +25,15 @@ def bi_search2(lis, num):
         elif lis[mid] > num:
             right = mid
     
-    #在这里返回
+    # 在这里返回
     if lis[left] == num:
         return left
     if lis[right] == num:
         return right
     
-    return -1
+    return None
 
-
-num = int(input('please input a num:'))
-alist = [1,2,2,2,2,6,7,8]
-bi_search2(alist,num)
+if __name__ == '__main__':
+    num = int(input('please input a num:'))
+    my_list = [1,2,2,2,2,6,7,8]
+    bi_search2(my_list,num)
--- Search 查找/二分法/simple_persion.py
+++ Search 查找/二分法/simple_persion.py
@@ -1,31 +1,34 @@
 '''
-简单方法
 二分法查找在列表中的用户输入值，返回index
-三种情况跳出循环体： LR相邻    LR位置重合   RL
+三种情况跳出循环体： LR相邻、LR位置重合、RL
 算法时间复杂度为O(logn)
-
 '''
 
 def bi_search(lis,num):
-    
-    if len(lis) == 0:           #判断边界条件
+
+    if len(lis) == 0:           # 判断边界条件
         return -1
-　　
-    left, right = 0, len(lis)-1          #列表的起始点和终点
-  
+
+    left, right = 0, len(lis)-1          # 列表的起始点和终点index
+
     while left <= right:
+        # 取二分中心点，如果(left+right)为偶数, python自动将mid向下取整
+        # 或 mid = (left + (right - left)) // 2
+        mid = (left + right) // 2
+        guess = lis[mid]
 
-        mid = (left + (right - left)) // 2     #取二分中心点，或 mid = (left + right) // 2
-
-        if num < lis[mid]:               #右点左移
-            right = mid - 1 
-        elif num > lis[mid]:             #左点右移
-            left = mid + 1 
-        else:                            #num == lis[mid]    
+        if guess == num:
             return mid
+        if guess < num:          # 猜小了，左点右移
+            left = mid + 1
+        else: # guess > num      # 猜大了，右点左移
+            right = mid -1
 
-    return -1
+    return None                  # 没有指定元素
 
-num = int(input('please input a num:'))
-alist = [1,2,3,4,5,6,7,8]
-bi_search(alist,num)
+# 测试
+if __name__ == '__main__':
+
+    num = int(input('please input a num:'))
+    my_list = [1,2,3,4,5,6,7,8]
+    bi_search(my_list,num)
